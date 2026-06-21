3c23a98b1669a98ca0eaa4251e23ceec7ba1e171
ongshu8027-svg <ongshu8027@gmail.com>
2026-06-21 15:02:11 +0900

    Update Run.java

--- java_002/README.md/Doll.java/Run.java
+++ java_002/README.md/Doll.java/Run.java
@@ -1,1 +1,14 @@
+public class run { 
+    public static void main(String[] args) {
+        Doll teddyBear = new Doll("테디", "곰");
+        Doll bunny = new Doll("버니", "토끼");
 
+        System.out.println("=== 곰인형 테디 ===");
+        teddyBear.introduce();
+        teddyBear.hug();
+        teddyBear.introduce();
+
+        System.out.println("\n=== 토끼인형 버니 ===");
+        bunny.introduce();
+    }
+}
